cd6cda6558cf1fa1681ffe8f35878c1306e1cad2
awangelo <anggg.gaio@gmail.com>
2024-10-18 23:11:49 -0300

    feat: completed attribute with Default Builder

--- src/main/java/com/github/awangelo/taskmanager/model/Task.java
+++ src/main/java/com/github/awangelo/taskmanager/model/Task.java
@@ -1,12 +1,18 @@
 package com.github.awangelo.taskmanager.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // Anotações para transformar a classe em uma entidade JPA
 @Entity
 @Table(name = "tasks")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,7 +20,8 @@ public class Task {
 
     private String title;
     private Priority priority;
-    private boolean completed;
+    @Builder.Default
+    private boolean completed = false;
 
     public enum Priority {
         LOW,
